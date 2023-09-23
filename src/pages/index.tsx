975864ae8abaee02df4551492ee038868bb252aa
chelproc <chelproc@outlook.com>
2023-09-23 16:51:44 +0900

    change settings for the festival

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,42 +1,103 @@
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Heading from '@theme/Heading';
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import Heading from "@theme/Heading";
 
-import styles from './index.module.css';
-
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
+const activities = [
+  {
+    title: "はじめてのプログラミング教室",
+    coverImage: {
+      src: "https://source.unsplash.com/hLvQ4-QEBAE/800x300",
+      alt: "ブロックプログラミング",
+    },
+    description:
+      // prettier-ignore
+      <p>
+        「はじめてのプログラミング教室」は ut.code(); がこれまでの学園祭でも提供してきた、自慢のコンテンツです。
+        ブロックを並べて、積み木感覚で、誰でも簡単にプログラミングに触れることができます。
+        でも最適化するとなると大人でも大変……。ぜひ挑戦していってください！
+      </p>,
+    linkTo: "https://festival.utcode.net/",
+  },
+  {
+    title: "Code vs Code",
+    coverImage: {
+      src: "https://source.unsplash.com/yWGiyL8DSys/800x300",
+      alt: "プログラムを書いて戦う様子",
+    },
+    description:
+      // prettier-ignore
+      <p>
+        ブロックをならべて、つなげて、自分だけのパイロット AI をプログラムしよう！
+        作ったプログラムで、企画に参加してくださった他の皆さんと対戦できます。
+        最強 AI の座を手にするのは一体誰だ！？
+      </p>,
+    linkTo: "https://code-vs-code.utcode.net/",
+  },
+  {
+    title: "ハッカーになろう",
+    coverImage: {
+      src: "https://source.unsplash.com/FnA5pAzqhMM/800x300",
+      alt: "セキュリティのイメージ",
+    },
+    description:
+      // prettier-ignore
+      <p>
+        悪用厳禁！Web サービスを攻撃して、隠された秘密のデータを盗み出そう！
+        実際に使われたことのあるサイバー攻撃手法を習得して、あなたもついにハッカーデビュー！？
+        インターネット上の詐欺から身を守る方法も学べます。
+      </p>,
+    linkTo: "/security",
+  },
+];
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
+    <Layout>
+      <header className="hero hero--primary">
+        <div className="container">
+          <Heading as="h1" className="hero__title">
+            {siteConfig.title}
+          </Heading>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+        </div>
+      </header>
+      <main className="container padding-vert--lg">
+        <ul
+          className="row row--no-gutters"
+          style={{
+            padding: 0,
+            listStyleType: "none",
+          }}
+        >
+          {activities.map((activity) => (
+            <li key={activity.title} className="col col--6 padding--md">
+              <div className="card" style={{ height: "100%" }}>
+                <div className="card__image">
+                  <img
+                    style={{ width: "100%" }}
+                    src={activity.coverImage.src}
+                    alt={activity.coverImage.alt}
+                  />
+                </div>
+                <div className="card__body">
+                  <h3>{activity.title}</h3>
+                  {activity.description}
+                </div>
+                <div className="card__footer">
+                  <a
+                    href={activity.linkTo}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="button button--primary button--block"
+                  >
+                    この企画を体験する
+                  </a>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
       </main>
     </Layout>
   );
