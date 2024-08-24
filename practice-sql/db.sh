000141674b89116afa3744d2df8cfa122fd5643f
aster <137767097+aster-void@users.noreply.github.com>
2024-08-24 16:14:27 +0900

    add DATABASE_URL and fix username

--- practice-sql/db.sh
+++ practice-sql/db.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
+# DATABASE_URL=postgres://user:password@localhost:5432/db
 docker run \
   -p "5432:5432" \
   -e POSTGRES_PASSWORD=password \
-  -e POSTGRES_USER=usr \
+  -e POSTGRES_USER=user \
   -e POSTGRES_DATABASE=db \
   postgres:16.4-alpine3.20
 
