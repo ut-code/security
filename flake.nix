5c80ea75317f61b5e787e8116bfec91844c6f4ba
aster <137767097+aster-void@users.noreply.github.com>
2024-10-19 00:51:08 +0900

    install biome on bun (as a pacman) and lefthook

--- flake.nix
+++ flake.nix
@@ -12,19 +12,16 @@
         devShell = pkgs.mkShell {
           nativeBuildInputs = [ pkgs.bashInteractive ];
           buildInputs = with pkgs; [
-            nodejs-slim_22
             nodePackages."@astrojs/language-server"
             biome
             bun
             go
             gnumake
+            lefthook
           ];
 
-          shellHook = with pkgs; ''
-            export PRISMA_SCHEMA_ENGINE_BINARY="${prisma-engines}/bin/schema-engine"
-            export PRISMA_QUERY_ENGINE_BINARY="${prisma-engines}/bin/query-engine"
-            export PRISMA_QUERY_ENGINE_LIBRARY="${prisma-engines}/lib/libquery_engine.node"
-            export PRISMA_FMT_BINARY="${prisma-engines}/bin/prisma-fmt"
+          shellHook = ''
+            lefthook install
           '';
         };
       });
