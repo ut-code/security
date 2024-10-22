f393798fd5970e36eda75ac1d502b6e009566e01
aster <137767097+aster-void@users.noreply.github.com>
2024-10-22 14:40:32 +0900

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,26 +16,26 @@ export default defineConfig({
     },
   },
   integrations: [starlight({
-    title: 'My Docs',
+    title: 'ハッカーになろう',
     social: {
-      github: 'https://github.com/withastro/starlight',
+      github: 'https://github.com/ut-code/security',
     },
     sidebar: [
-      {
-        label: 'XSS',
-        slug: "xss"
-        // you can also do something like this:
-        // items: [
-        //   // Each item here is one entry in the navigation menu.
-        //   { label: 'Example Guide', slug: 'guides/example' },
-        // ],
-        // or like this.
-        // autogenerate: { directory: 'reference' },
-      },
       {
         label: 'SQL インジェクション',
         slug: "sql-injection"
       },
+      {
+        label: 'XSS',
+        slug: "xss"
+      },
+      // you can also do something like this:
+      // items: [
+      //   // Each item here is one entry in the navigation menu.
+      //   { label: 'Example Guide', slug: 'guides/example' },
+      // ],
+      // or like this.
+      // autogenerate: { directory: 'reference' },
     ],
   }), preact({ compat: true }), svelte(),],
 });
