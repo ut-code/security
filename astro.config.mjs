b201a1da4bc935da4882810126b9ea08c39ff519
aster <137767097+aster-void@users.noreply.github.com>
2024-11-01 16:18:34 +0900

    install tailwind and daisyui

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import starlight from "@astrojs/starlight";
 import preact from "@astrojs/preact";
 import svelte from "@astrojs/svelte";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -18,32 +20,28 @@ export default defineConfig({
     defaultLocale: "ja",
     locales: ["ja"],
   },
-  integrations: [
-    starlight({
-      title: "ハッカーになろう",
-      defaultLocale: "ja",
-      social: {
-        github: "https://github.com/ut-code/security",
+  integrations: [starlight({
+    title: "ハッカーになろう",
+    defaultLocale: "ja",
+    social: {
+      github: "https://github.com/ut-code/security",
+    },
+    sidebar: [
+      {
+        label: "SQL インジェクション",
+        slug: "sql-injection",
       },
-      sidebar: [
-        {
-          label: "SQL インジェクション",
-          slug: "sql-injection",
-        },
-        {
-          label: "XSS",
-          slug: "xss",
-        },
-        // you can also do something like this:
-        // items: [
-        //   // Each item here is one entry in the navigation menu.
-        //   { label: 'Example Guide', slug: 'guides/example' },
-        // ],
-        // or like this.
-        // autogenerate: { directory: 'reference' },
-      ],
-    }),
-    preact(),
-    svelte(),
-  ],
+      {
+        label: "XSS",
+        slug: "xss",
+      },
+      // you can also do something like this:
+      // items: [
+      //   // Each item here is one entry in the navigation menu.
+      //   { label: 'Example Guide', slug: 'guides/example' },
+      // ],
+      // or like this.
+      // autogenerate: { directory: 'reference' },
+    ],
+  }), preact(), svelte(), tailwind()],
 });
