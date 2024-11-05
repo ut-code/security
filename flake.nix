7bd1202c2930434d7fc98737029debb00e4d48d6
aster <137767097+aster-void@users.noreply.github.com>
2024-11-05 19:17:41 +0900

    exp: use worker-rs

--- flake.nix
+++ flake.nix
@@ -1,17 +1,23 @@
 {
   description = "ut.code(); 五月祭・駒場祭企画 「ハッカーになろう」";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { nixpkgs, flake-utils, ... }:
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    flake-utils.url = "github:numtide/flake-utils";
+    fenix.url = "github:nix-community/fenix";
+  };
+
+  outputs = { nixpkgs, flake-utils, fenix, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        toolchain = import ./services { inherit system fenix; };
       in
       {
         devShell = pkgs.mkShell {
           nativeBuildInputs = [ pkgs.bashInteractive ];
           buildInputs = with pkgs; [
+            toolchain
             nodejs_22
             bun
             biome
