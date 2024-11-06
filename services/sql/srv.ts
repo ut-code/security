6b837f9e36a2d291648e7ea4834be310e1f5a9cf
aster <137767097+aster-void@users.noreply.github.com>
2024-11-06 20:10:27 +0900

    install better-sqlite3

--- services/sql/srv.ts
+++ services/sql/srv.ts
@@ -1,24 +1,30 @@
-const { Database } = await import("bun:sqlite").catch(console.warn); // wrangler can't identify this at build time
+import Database from "better-sqlite3";
 import type { Mail } from "./types.dev";
 
-const dbName = `${import.meta.dir}/sqlite.db`;
-const db = new Database(dbName);
+const fallback = __filename.split("/");
+fallback.pop();
+const dbName = `${import.meta.dir ?? fallback.pop()}/sqlite.db`;
+const db = new Database(dbName, {
+  readonly: true,
+});
 const username = "駒場 優";
 
 export function from(from: string) {
   return db
-    .query(
+    .prepare(
       `SELECT * FROM mails WHERE "to" = '${username}' AND "from" = '${from}' ORDER BY date DESC`,
     )
     .all() as Mail[];
 }
 export function all() {
   return db
-    .query(`SELECT * FROM mails WHERE "to" = '${username}' ORDER BY date DESC`)
+    .prepare(
+      `SELECT * FROM mails WHERE "to" = '${username}' ORDER BY date DESC`,
+    )
     .all() as Mail[];
 }
 
 // don't expose this to users
 export function everything() {
-  return db.query("SELECT * FROM mails").all() as Mail[];
+  return db.prepare("SELECT * FROM mails").all() as Mail[];
 }
