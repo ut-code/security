9d530eff0ec9c94a8d4b109f84db1148457f0f65
aster <137767097+aster-void@users.noreply.github.com>
2024-11-06 15:59:22 +0900

    delete rust

--- services/README.md
+++
@@ -1,1 +0,0 @@
-https://github.com/cloudflare/workers-rs
--- services/default.nix
+++
@@ -1,6 +0,0 @@
-{ system, fenix }:
-fenix.packages.${system}.fromToolchainFile {
-  file = ./rust-toolchain.toml;
-  # just copy the hash for now and nix will tell you the right hash
-  sha256 = "sha256-yMuSb5eQPO/bHv+Bcf/US8LVMbf/G/0MSfiPwBhiPpk=";
-}
