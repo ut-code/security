20b5a0565187614b6bf329244a8e5d142108186a
aster <137767097+aster-void@users.noreply.github.com>
2024-11-09 13:11:12 +0900

    changes

--- src/apps/svelte/cracker/checkers.ts
+++ src/apps/svelte/cracker/checkers.ts
@@ -1,7 +1,14 @@
-export type CheckerKind = "string eq";
+import * as sha from "js-sha256";
+import * as bcrypt from "bcryptjs";
+
+export type CheckerKind = "string eq" | "sha256" | "bcrypt";
 export function check(alg: CheckerKind, src: string, hash: string) {
   switch (alg) {
     case "string eq":
       return src === hash;
+    case "sha256":
+      return sha.sha256(src) === hash;
+    case "bcrypt":
+      return;
   }
 }
